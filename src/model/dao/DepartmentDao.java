5187d03f6b7613fd4fe66dd8b6fb8169a1f0c77f
Rafael Santos <rafael_16001@hotmail.com>
2021-07-20 12:05:20 -0300

    adding database access

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findbyId(Integer id);
+	List<Department> findAll();
+	
+
+}
